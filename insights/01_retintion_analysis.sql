e6c78d9b823702881319989783f6b74850b24943
aal_govind <aalgovind05@gmail.com>
2026-04-20 11:55:33 +0530

    Add Q1 customer retention analysis

---
+++ insights/01_retintion_analysis.sql
@@ -0,0 +1,26 @@
+--BUSINESS QUESTION*1. Customer Retention Analysis*
+--Which customers ordered more than once? What's the gap between their first and last order?
+-- Techniques: CTE + MIN() / MAX() + HAVING
+
+
+ WITH order_info AS (
+    SELECT c.contact_name,
+        c.customer_id,
+        c.country,
+        COUNT(o.order_id) total_order,
+        MIN(o.order_date) AS first_order_date,
+        MAX(o.order_date) AS last_order_date,
+        MAX(o.order_date) - MIN(o.order_date) AS date_gap
+    FROM customers c 
+    JOIN orders o
+        ON c.customer_id = o.customer_id
+    GROUP BY c.contact_name,
+        c.customer_id,
+        c.country
+    HAVING COUNT(o.order_id) > 1
+ )
+
+SELECT *
+FROM order_info
+ORDER BY date_gap DESC
+
