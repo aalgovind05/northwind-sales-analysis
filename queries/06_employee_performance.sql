4573965f8cba4256ecbaf4017d68391512bb778b
aal_govind <aalgovind05@gmail.com>
2026-04-26 10:19:51 +0530

    employees performance and coaching need

---
+++ queries/06_employee_performance.sql
@@ -0,0 +1,33 @@
+-- BUSINESS QUESTION 6.Employee & Operational Performance
+--Employee performance vs team average
+--Which employees are above average performers? Who needs coaching based on revenue generated?
+
+
+WITH employee_revenue AS(
+    SELECT 
+        CONCAT(e.first_name,' ',e.last_name) AS employee_name,
+        ROUND(SUM(od.unit_price*od.quantity - od.discount):: NUMERIC,2) AS emp_total_revenue
+    FROM order_details od
+    JOIN orders o
+    ON od.order_id = o.order_id
+    JOIN employees e
+    ON o.employee_id = e.employee_id
+    GROUP BY CONCAT(e.first_name,' ',e.last_name)
+    ),
+
+avg_team_revenue AS
+(SELECT
+AVG(emp_total_revenue) AS team_revenue
+FROM employee_revenue)
+
+SELECT employee_name,
+        emp_total_revenue,
+        team_revenue,
+            CASE 
+                WHEN emp_total_revenue >= team_revenue
+                THEN 'above average'
+                ELSE 'coaching needs' END AS performance_status
+FROM employee_revenue
+CROSS JOIN avg_team_revenue
+ORDER BY emp_total_revenue DESC ;
+
---
+++ queries/07_shipping_delay_analysis.sql
@@ -0,0 +1,8 @@
+--BUSINESS QUESTION 7.Shipping delay analysis by country
+--Which countries experience the longest delivery times? Where are fulfillment bottlenecks occurring?
+/*CTE
+AVG()
+DATE diff
+Operations
+Advanced
+07_shipping_delay_analysis.sql*/
