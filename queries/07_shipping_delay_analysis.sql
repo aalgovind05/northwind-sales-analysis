c4c60d1377dac9c03fa6ffd83e04ca434ff1a3ad
aal_govind <aalgovind05@gmail.com>
2026-04-28 16:25:32 +0530

    shipping delay analysis

--- queries/07_shipping_delay_analysis.sql
+++ queries/07_shipping_delay_analysis.sql
@@ -3,6 +3,14 @@
 /*CTE
 AVG()
 DATE diff
-Operations
-Advanced
-07_shipping_delay_analysis.sql*/
+Operations*/
+
+
+SELECT ship_country,
+    ROUND(AVG(shipped_date - order_date) :: NUMERIC)AS AVG_day_to_ship,
+        COUNT(CASE 
+            WHEN (shipped_date - order_date) > 10 THEN 1 END) AS delayed_orders
+FROM orders
+WHERE shipped_date IS NOT NULL
+GROUP BY ship_country
+ORDER BY AVG_day_to_ship DESC
